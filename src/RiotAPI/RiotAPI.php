f2e13cc1a381a7166448cd0e54616218d71f6eb8
MiguelFreire <m.b.freire@hotmail.com>
2013-12-13 20:06:07 +0000

    Update: v1.2
    - Now you can set your default region by: $foo->setRegion('euw');
    - Improved error handling

--- src/RiotAPI/RiotAPI.php
+++ src/RiotAPI/RiotAPI.php
@@ -6,13 +6,13 @@ class RiotAPI {
 
 	//Developer API KEY or APP KEY
 
-	protected $key = "YOUR-API-KEY";
+	protected $key;
 
 	protected $server_v1 = 'http://prod.api.pvp.net/api/lol/{region}/v1.1';
 
 	protected $server_v2 = 'http://prod.api.pvp.net/api/{region}/v2.1';
  		
-
+	protected $region;
 
 	/**
      * Get Champions list
@@ -20,24 +20,23 @@ class RiotAPI {
      * @param 	boolean		$free
      * @return  array
     */
-	public function getChampions($free = false, $region = "euw") {
+	public function getChampions($free = false, $region = null) {
+		$region = (is_null($region)) ? $this->region : $region;
 		$this->validate_region('champion', $region);
 		if($free) {
 			$call = $this->server_v1 . "/champion/?freeToPlay=true&";
 			$result = $this->request($region,$call);
-			$data = json_decode($result,true);
-				
-				return $data['champions'];
+			
+			return $result;
 
 		} elseif(!$free) {
 			$call = $this->server_v1."/champion/?";
 			$result = $this->request($region,$call);
-			$data = json_decode($result,true);
-				
-				return $data['champions'];
+			
+			return $result;
 
 		} else {
-			throw new Exception("$free must be a boolean", 1);
+			throw new \Exception("The parameter must be a boolean", 1);
 		}
 	}
 
@@ -48,21 +47,20 @@ class RiotAPI {
      * @param 	string		$region
      * @return  array
     */
-	public function getSummonerBy($summoner, $get_by = "id",  $region = "euw") {
+	public function getSummonerBy($summoner, $get_by = "id",  $region = null) {
+		$region = (is_null($region)) ? $this->region : $region;
 		$this->validate_region('summoner', $region);
 		if($get_by == "id") {
 			$call = $this->server_v1 . "/summoner/" . $summoner . "?";
-			$result = $this->request($region, $call);
-			$data = json_decode($result, true);
+			$result = $this->request($region, $call);		
 		} elseif($get_by == "name") {
 			$call = $this->server_v1 . "/summoner/by-name/" . $summoner . "?";
 			$result = $this->request($region, $call); 
-			$data = json_decode($result, true);
 		} else {
-			throw new Exception('You can only get summoners by ID or Name!', 1);
+			throw new \Exception('You can only get summoners by ID or Name!', 1);
 		}
 
-		return $data;
+		return $result;
 	}
 
 	/**
@@ -72,7 +70,8 @@ class RiotAPI {
      * @param 	string		$region
      * @return  array
     */
-	public function getSummoner($summoner_id, $options, $region = "euw") {
+	public function getSummoner($summoner_id, $options, $region = null) {
+		$region = (is_null($region)) ? $this->region : $region;
 		$this->validate_region('summoner', $region);
 		$call = $this->server_v1 . "/summoner/" . $summoner_id;
 		switch ($options) {
@@ -88,13 +87,12 @@ class RiotAPI {
 				$call .= "/name/?";
 				break;	
 			default:
-				throw new Exception('Get summoner '.$options.' not available',1);
+				throw new \Exception('Get summoner '.$options.' not available',1);
 				break;
 		}
 		$result = $this->request($region , $call);
-		$data = json_decode($result, true);
-
-		return $data;
+		
+		return $result;
 	}
 
 	/**
@@ -103,14 +101,13 @@ class RiotAPI {
      * @param 	string		$region
      * @return  array
     */
-	public function getRecentGames($summoner_id, $region = "euw") {
+	public function getRecentGames($summoner_id, $region = null) {
+		$region = (is_null($region)) ? $this->region : $region;
 		$this->validate_region('game', $region);
 		$call = $this->server_v1."/game/by-summoner/" . $summoner_id . "/recent/?";
 		$result = $this->request($region, $call);
 
-		$data = json_decode($result, true);
-
-		return $data;
+		return $result;
 	}
 
 	/**
@@ -119,13 +116,13 @@ class RiotAPI {
      * @param 	string		$region
      * @return  array
     */
-	public function getLeagues($summoner_id, $region = "euw") {
+	public function getLeagues($summoner_id, $region = null) {
+		$region = (is_null($region)) ? $this->region : $region;
 		$this->validate_region('league', $region);
 		$call = $this->server_v2."/league/by-summoner/" . $summoner_id . "?";
 		$result = $this->request($region, $call);
-		$data = json_decode($result, true);
-
-		return $data;
+		
+		return $result;
 	}
 
 	/**
@@ -136,25 +133,24 @@ class RiotAPI {
      * @param 	string		$region
      * @return  array
     */
-	public function getPlayerStats($summoner_id, $options = "summary", $season = "SEASON3",  $region ="euw") {
+	public function getPlayerStats($summoner_id, $options = "summary", $season = "SEASON3",  $region = null) {
+		$region = (is_null($region)) ? $this->region : $region;
 		$this->validate_region('stats', $region);
 		switch ($options) {
 			case 'summary':
 				$call = $this->server_v1 . "/stats/by-summoner/" . $summoner_id . "/summary?season=" . $season . "&";
 				$result = $this->request($region, $call);
-				$data = json_decode($result, true);
 				break;
 			case 'ranked':
 				$call = $this->server_v1 . "/stats/by-summoner/" . $summoner_id . "/ranked?season=" . $season . "&";
 				$result = $this->request($region, $call);
-				$data = json_decode($result, true);
 				break;
 			default:
-				throw new Exception('No option available', 1);
+				throw new \Exception('No option available', 1);
 				break;
 		}
 			
-			return $data;
+			return $result;
 	}
 
 	/**
@@ -163,13 +159,13 @@ class RiotAPI {
      * @param 	string		$region
      * @return  array
     */
-	public function getTeam($summoner_id, $region = "euw") {
+	public function getTeam($summoner_id, $region = null) {
+		$region = (is_null($region)) ? $this->region : $region;
 		$this->validate_region('team', $region);
 		$call = $this->server_v2 . "/team/by-summoner/" . $summoner_id . "?";
 		$result = $this->request($region, $call);
-		$data = json_decode($result, true);
 		$this->validate_region('champion', 'euw');
-		return $data;
+		return $result;
 	}
 
 	/**
@@ -177,8 +173,17 @@ class RiotAPI {
      * @param   string 		$key
      * @return  void
     */
-	public function setKey($key) {
+	public function setKey($key = null) {
 		$this->key = $key;
+	}
+
+	/**
+     * Set region
+     * @param   string 		$region
+     * @return  void
+    */
+	public function setRegion($region = null) {
+		$this->region = $region;
 	}
 
 	/**
@@ -190,15 +195,21 @@ class RiotAPI {
 	private function request($region, $call) {
 		$request = str_replace('{region}', $region, $call) . 'api_key=' . $this->key;
 		$ch = curl_init();
-		// Disable SSL verification
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		// Will return the response, if false it print the response
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		// Set the url
 		curl_setopt($ch, CURLOPT_URL,$request);
-		// Execute
+
 		$result=curl_exec($ch);
-		return $result;
+		$data = json_decode($result,true);
+		
+		if(is_null($data)) return $data;
+		
+		if(array_key_exists('status', $data)) {
+			throw new \Exception($data['status']['status_code'] . ' - ' . $data['status']['message'], 1);
+		}
+
+		return $data;
+
 	}
 
 	/**
@@ -226,7 +237,7 @@ class RiotAPI {
 		if(array_key_exists($queueID, $queues)) {
 			return $queues[$queueID];
 		}
-		else throw new Exception('No queue with that QueueID', 1);
+		else throw new \Exception('No queue with that QueueID', 1);
 	}
 	
 	/**
@@ -280,20 +291,23 @@ class RiotAPI {
 
 		);
 
+		$regions_list = array('euw','eune','na','tr','br');
+
+		if(!in_array($region, $regions_list)) throw new \Exception("Region Not Found", 1);
+		
+
 		if (array_key_exists($module, $reference)) {
 			$reference_region = explode("|", $reference[$module]['regions']);
-			if(!in_array($region, $reference_region)) throw new Exception("Error Processing Request", 1);
+			if(!in_array($region, $reference_region)) throw new \Exception("The current function (".$module.") is not available in region (".$region.")", 1);
 			
 
 		} else {
-			throw new Exception("Module not found", 1);
+			throw new \Exception("Module not found", 1);
 			
 		}
 
 	
 	}
 
-
-	
-
 }
+
